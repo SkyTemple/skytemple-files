2e76092c6b1b84b944d12c4ace26ea9ae4ecc1ea
Parakoopa <parakoopa@live.de>
2020-01-12 21:10:11 +0100

    Removes debugging code for previous commit

--- skytemple_files/compression/bma_layer_nrl/handler.py
+++ skytemple_files/compression/bma_layer_nrl/handler.py
@@ -15,19 +15,7 @@ class BmaLayerNrlHandler:
         Second return is compressed original size.
         Returns 16 bit LE integer list. Input is compressed as pair24 (pairs of two 12 bit ints).
         """
-        with open('/tmp/before.bin', 'wb') as f:
-            f.write(compressed_data)
-        data = BmaLayerNrlDecompressor(compressed_data, stop_when_size).decompress()
-        with open('/tmp/before_un.bin', 'wb') as f:
-            f.write(data[0])
-        new_compressed = cls.compress(data[0])
-        with open('/tmp/after.bin', 'wb') as f:
-            f.write(new_compressed)
-        after_data = BmaLayerNrlDecompressor(new_compressed, stop_when_size).decompress()
-        with open('/tmp/after_un.bin', 'wb') as f:
-            f.write(after_data[0])
-        assert data[0] == after_data[0]
-        return data
+        return BmaLayerNrlDecompressor(compressed_data, stop_when_size).decompress()
 
     @classmethod
     def compress(cls, uncompressed_data: bytes) -> bytes:
--- skytemple_files/compression/bpc_image/handler.py
+++ skytemple_files/compression/bpc_image/handler.py
@@ -11,25 +11,7 @@ class BpcImageHandler:
     @classmethod
     def decompress(cls, compressed_data: bytes, stop_when_size: int) -> Tuple[bytes, int]:
         """todo. Stops when stop_when_size bytes have been decompressed."""
-        with open('/tmp/before.bin', 'wb') as f:
-            f.write(compressed_data)
-        data = BpcImageDecompressor(compressed_data, stop_when_size).decompress()
-        with open('/tmp/before_un.bin', 'wb') as f:
-            f.write(data[0])
-        new_compressed = cls.compress(data[0])
-        with open('/tmp/after.bin', 'wb') as f:
-            f.write(new_compressed)
-        de = BpcImageDecompressor(new_compressed, stop_when_size)
-        try:
-            after_data = de.decompress()
-            with open('/tmp/after_un.bin', 'wb') as f:
-                f.write(after_data[0])
-        except ValueError as e:
-            with open('/tmp/after_un.bin', 'wb') as f:
-                f.write(de.decompressed_data)
-            raise e
-        assert data[0] == after_data[0]
-        return data
+        return BpcImageDecompressor(compressed_data, stop_when_size).decompress()
 
     @classmethod
     def compress(cls, uncompressed_data: bytes) -> bytes:

ee2d252ca38be2f3957d53ea11829c50ba5388fa
Parakoopa <parakoopa@live.de>
2020-01-28 22:04:31 +0100

    setup.py and readme

---
+++ README.rst
@@ -0,0 +1,7 @@
+SkyTemple Files
+===============
+
+This is still very WIP, so I didn't feel like
+putting a proper README here! But you'll find
+README's containing the documentation for file types
+in the directories for the file handlers.
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# README read-in
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+# END README read-in
+
+setup(
+    name='skytemple-files',
+    version='0.0.1',
+    packages=find_packages(),
+    description='Python library to edit the ROM of Pokémon Mystery Dungeon Explorers of Sky (EU/US)',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/SkyTemple/skytemple-files/',
+    install_requires=[
+        # TODO
+    ],
+    classifiers=[
+        # TODO
+    ],
+)

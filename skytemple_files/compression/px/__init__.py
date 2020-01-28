c51e3d908fc2fa483cb73fc95827f177f52d91fd
Parakoopa <parakoopa@live.de>
2020-01-28 21:34:34 +0100

    LICENSE

--- skytemple_files/compression/px/__init__.py
+++ skytemple_files/compression/px/__init__.py
@@ -1,3 +1,20 @@
+#  Copyright 2020 Parakoopa
+#
+#  This file is part of SkyTemple.
+#
+#  SkyTemple is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SkyTemple is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+
 # Length of the default lookback buffer. The "sliding window" so to speak!
 # Used to determine how far back the compressor looks for matching sequences!
 PX_LOOKBACK_BUFFER_SIZE = 4096

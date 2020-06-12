7d7dd4a6ccbfb4755ea79d4655929d8e4ce2410d
Parakoopa <parakoopa@live.de>
2020-06-12 23:39:18 +0200

    Listing patches, Windows fix

--- skytemple_files/graphics/wan_wat/model.py
+++ skytemple_files/graphics/wan_wat/model.py
@@ -19,7 +19,7 @@ from typing import List, Tuple
 try:
     from PIL import Image, ImageOps
 except ImportError:
-    from pil import Image
+    from pil import Image, ImageOps
 from skytemple_rust.pmd_wan import WanImage, MetaFrameGroup, MetaFrame, Animation
 
 

6c0a3fe313e01f3e1da32e50eb0ee3f32f2866aa
Parakoopa <parakoopa@live.de>
2020-07-12 11:54:31 +0200

    ExplorerScript version bump to 0.0.5

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'ndspy >= 3.0.0',
         'Pillow >= 6.1.0',
         'appdirs >= 1.4.0',
-        'explorerscript >= 0.0.4',
+        'explorerscript >= 0.0.5',
         'skytemple-rust >= 0.0.1',
         'tilequant >= 0.0.1',
         'typing-extensions == 3.7.4.2; python_version<"3.7"',

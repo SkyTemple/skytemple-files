0c6b2bafae7f5ac1dc14a15a555976d383c42cd4
Parakoopa <parakoopa@live.de>
2020-09-18 11:29:40 +0200

    Hardcoded: Editing the rank up table (#14)

---
+++ skytemple_files/hardcoded/dbg/rank_up_table_test.py
@@ -0,0 +1,36 @@
+#  Copyright 2020 Parakoopa
+# 
+#  This file is part of SkyTemple.
+# 
+#  SkyTemple is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  SkyTemple is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+import os
+
+from ndspy.rom import NintendoDSRom
+
+from skytemple_files.common.util import get_ppmdu_config_for_rom, get_binary_from_rom_ppmdu
+from skytemple_files.hardcoded.rank_up_table import HardcodedRankUpTable
+
+base_dir = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..')
+rom_us = NintendoDSRom.fromFile(os.path.join(base_dir, 'skyworkcopy_us.nds'))
+ppmdu_us = get_ppmdu_config_for_rom(rom_us)
+arm9_us = get_binary_from_rom_ppmdu(rom_us, ppmdu_us.binaries['arm9.bin'])
+
+rank_up = HardcodedRankUpTable.get_rank_up_table(arm9_us, ppmdu_us)
+
+for i, rank in enumerate(rank_up):
+    print(i, rank)
+    
+# Try setting and see if still same.
+HardcodedRankUpTable.set_rank_up_table(rank_up, arm9_us, ppmdu_us)
+assert rank_up == HardcodedRankUpTable.get_rank_up_table(arm9_us, ppmdu_us)
---
+++ skytemple_files/hardcoded/rank_up_table.py
@@ -0,0 +1,73 @@
+#  Copyright 2020 Parakoopa
+#
+#  This file is part of SkyTemple.
+#
+#  SkyTemple is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SkyTemple is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+from typing import List
+
+from skytemple_files.common.ppmdu_config.data import Pmd2Data
+from skytemple_files.common.util import read_uintle, write_uintle, AutoString
+
+ENTRY_LEN = 16
+
+
+class Rank(AutoString):
+    def __init__(self, rank_name_str: int, points_needed_next: int, storage_capacity: int, item_awarded: int):
+        self.rank_name_str = rank_name_str
+        self.points_needed_next = points_needed_next
+        self.storage_capacity = storage_capacity
+        self.item_awarded = item_awarded
+
+    def to_bytes(self) -> bytes:
+        buffer = bytearray(ENTRY_LEN)
+        write_uintle(buffer, self.rank_name_str, 0x00, 4)
+        write_uintle(buffer, self.points_needed_next, 0x04, 4)
+        write_uintle(buffer, self.storage_capacity, 0x08, 4)
+        write_uintle(buffer, self.item_awarded, 0x0C, 4)
+        return buffer
+
+    def __eq__(self, other):
+        if not isinstance(other, Rank):
+            return False
+        return self.rank_name_str == other.rank_name_str and self.points_needed_next == other.points_needed_next \
+                and self.storage_capacity == other.storage_capacity and self.item_awarded == other.item_awarded
+
+
+class HardcodedRankUpTable:
+    @classmethod
+    def get_rank_up_table(cls, arm9bin: bytes, config: Pmd2Data) -> List[Rank]:
+        """Returns the list of ranks in the game."""
+        block = config.binaries['arm9.bin'].blocks['RankUpTable']
+        lst = []
+        for i in range(block.begin, block.end, ENTRY_LEN):
+            lst.append(Rank(
+                read_uintle(arm9bin, i + 0x00, 4),
+                read_uintle(arm9bin, i + 0x04, 4),
+                read_uintle(arm9bin, i + 0x08, 4),
+                read_uintle(arm9bin, i + 0x0C, 4)
+            ))
+        return lst
+
+    @classmethod
+    def set_rank_up_table(cls, value: List[Rank], arm9bin: bytearray, config: Pmd2Data):
+        """
+        Sets the list of ranks in the game.
+        The length of the list must exactly match the original ROM's length (see get_rank_up_table).
+        """
+        block = config.binaries['arm9.bin'].blocks['RankUpTable']
+        expected_length = int((block.end - block.begin) / ENTRY_LEN)
+        if len(value) != expected_length:
+            raise ValueError(f"The list must have exactly the length of {expected_length} entries.")
+        for i, entry in enumerate(value):
+            arm9bin[block.begin + (i * ENTRY_LEN):block.begin + ((i + 1) * ENTRY_LEN)] = entry.to_bytes()

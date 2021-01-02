c0edfee7864f64641dc64d08dac7c2edde4f96e8
irdkwia <irdkwia2000@outlook.com>
2021-01-02 23:48:33 +0100

    Copyright update 2021

--- skytemple_files/hardcoded/cart_removed.py
+++ skytemple_files/hardcoded/cart_removed.py
@@ -1,5 +1,5 @@
 """Module for editing hardcoded data regarding the cartridge removed image."""
-#  Copyright 2020 Parakoopa
+#  Copyright 2020-2021 Parakoopa and the SkyTemple Contributors
 #
 #  This file is part of SkyTemple.
 #

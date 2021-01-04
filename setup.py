606e586de0da1849331d8c3cca7863ad89c16df3
Parakoopa <parakoopa@live.de>
2021-01-04 21:33:47 +0100

    Chara WAN: Add Shadow.png to setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,9 @@ setup(
     name='skytemple-files',
     version=__version__,
     packages=find_packages(),
-    package_data={'skytemple_files': get_resources(['.xml', '.asm', '.rst', 'LICENSE', '.txt', 'md'])},
+    package_data={'skytemple_files':
+                  get_resources(['.xml', '.asm', '.rst', 'LICENSE', '.txt', 'md']) +
+                  ['graphics/chara_wan/Shadow.png']},
     description='Python library to edit the ROM of Pokémon Mystery Dungeon Explorers of Sky (EU/US)',
     long_description=long_description,
     long_description_content_type='text/x-rst',

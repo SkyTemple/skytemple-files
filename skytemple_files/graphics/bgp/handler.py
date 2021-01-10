2151913c5fc37158aa1d9b2d7d79d067aec8f928
irdkwia <43478422+irdkwia@users.noreply.github.com>
2021-01-10 23:35:01 +0100

    Remove AT4PN from compression choice list for BGPs

--- skytemple_files/graphics/bgp/handler.py
+++ skytemple_files/graphics/bgp/handler.py
@@ -18,7 +18,7 @@
 from skytemple_files.common.types.data_handler import DataHandler
 from skytemple_files.graphics.bgp.model import Bgp
 from skytemple_files.graphics.bgp.writer import BgpWriter
-from skytemple_files.compression_container.common_at.handler import COMMON_AT_BEST_4
+from skytemple_files.compression_container.common_at.handler import COMMON_AT_MUST_COMPRESS_4
 
 class BgpHandler(DataHandler[Bgp]):
     @classmethod
@@ -32,6 +32,6 @@ class BgpHandler(DataHandler[Bgp]):
         return FileType.COMMON_AT.serialize(
             FileType.COMMON_AT.compress(
                 BgpWriter(data).write(),
-                COMMON_AT_BEST_4
+                COMMON_AT_MUST_COMPRESS_4
             )
         )

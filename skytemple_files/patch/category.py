e5ca051cfaccf514f5eb1eb87137d45fa793c3f2
Parakoopa <parakoopa@live.de>
2021-04-25 13:30:34 +0200

    Fix locale import

--- skytemple_files/patch/category.py
+++ skytemple_files/patch/category.py
@@ -15,6 +15,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
 from enum import Enum, auto
+from skytemple_files.common.i18n_util import _
 
 
 class PatchCategory(Enum):

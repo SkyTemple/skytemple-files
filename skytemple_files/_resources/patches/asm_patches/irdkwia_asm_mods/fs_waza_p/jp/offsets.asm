619cdaccfef3c54879a031933b66b813e3cbc0cb
irdkwia <irdkwia2000@outlook.com>
2021-05-17 18:25:16 +0200

    Check for invalid waza data access

--- skytemple_files/_resources/patches/asm_patches/irdkwia_asm_mods/fs_waza_p/jp/offsets.asm
+++ skytemple_files/_resources/patches/asm_patches/irdkwia_asm_mods/fs_waza_p/jp/offsets.asm
@@ -12,6 +12,10 @@
 
 .definelabel FillWithZeros8Bytes, 0x02003228
 
+.definelabel FGetSize, 0x02008244
+
+.definelabel PrintF, 0x0200C1FC
+
 .definelabel LoadWazaP, 0x02013394
 .definelabel LoadWazaP2, 0x020133BC
 .definelabel UnloadCurrentWazaP, 0x020133E4
@@ -61,9 +65,10 @@
 .definelabel ExclusiveItemPPUp2, 0x020A2C9C
 .definelabel ExclusiveItemPPUp4, 0x020A2C48
 
-.definelabel TrueUnloadCurrentWazaP, 0x020A7268
-.definelabel ReadMoveValue, TrueUnloadCurrentWazaP+0x140
-.definelabel ReadMoveBuffer, ReadMoveValue+0xA0
+.definelabel LSSize, 0x800
+.definelabel TrueUnloadCurrentWazaP, 0x020A7A68 - LSSize
+.definelabel ReadMoveValue, TrueUnloadCurrentWazaP+0x120
+.definelabel ReadMoveBuffer, ReadMoveValue+0xC0
 .definelabel ReadMoveset, ReadMoveBuffer+0x1C
 .definelabel ReadMovesetBuffer, ReadMoveset+0xA0
 ; r0: result = ReadMoveValue(r0: move_id, r1: value_offset, r2: value_size)

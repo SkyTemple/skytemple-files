015f62432559c517f4ff373700260db9b9166db3
Parakoopa <parakoopa@live.de>
2021-06-09 00:37:58 +0200

    Rendering ground engine maps with dungeon tilesets

--- skytemple_files/hardcoded/ground_dungeon_tilesets.py
+++ skytemple_files/hardcoded/ground_dungeon_tilesets.py
@@ -15,10 +15,20 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
-from typing import List
+from typing import List, Optional, Tuple
 
 from skytemple_files.common.ppmdu_config.data import Pmd2Data
+from skytemple_files.common.ppmdu_config.script_data import Pmd2ScriptLevel, Pmd2ScriptLevelMapType
 from skytemple_files.common.util import read_uintle, write_uintle, AutoString, read_sintle
+from skytemple_files.container.dungeon_bin.model import DungeonBinPack
+from skytemple_files.dungeon_data.fixed_bin.model import FixedFloor, FixedBin
+from skytemple_files.dungeon_data.mappa_bin.model import MappaBin
+from skytemple_files.graphics.dma.model import Dma
+from skytemple_files.graphics.dpc.model import Dpc
+from skytemple_files.graphics.dpci.model import Dpci
+from skytemple_files.graphics.dpl.model import Dpl
+from skytemple_files.graphics.dpla.model import Dpla
+from skytemple_files.hardcoded.dungeons import DungeonDefinition
 
 
 class GroundTilesetMapping(AutoString):
@@ -79,3 +89,39 @@ class HardcodedGroundDungeonTilesets:
             raise ValueError(f"The list must have exactly the length of {expected_length} entries.")
         for i, entry in enumerate(value):
             overlay11bin[block.begin + i * 8:block.begin + (i + 1) * 8] = entry.to_bytes()
+
+
+def resolve_mapping_for_level(
+    level: Pmd2ScriptLevel, tileset_mappings: List[GroundTilesetMapping],
+    mappa: MappaBin, fixed: FixedBin, dungeon_bin: DungeonBinPack,
+    dungeons: List[DungeonDefinition]
+) -> Optional[Tuple[Dma, Dpc, Dpci, Dpl, Dpla, Optional[FixedFloor]]]:
+    """Returns tileset data and fixed floor data (if applicable) for the given level"""
+    if level.mapty_enum != Pmd2ScriptLevelMapType.FIXED_ROOM and level.mapty_enum != Pmd2ScriptLevelMapType.TILESET:
+        return None
+    dungeon_id, floor_id = None, None
+    for mapping in tileset_mappings:
+        if mapping.ground_level == level.id:
+            dungeon_id, floor_id = mapping.dungeon_id, mapping.floor_id
+            break
+    if not dungeon_id:
+        return None
+    mappa_idx = dungeons[dungeon_id].mappa_index
+    start_offset = dungeons[dungeon_id].start_after
+    length = dungeons[dungeon_id].number_floors
+    floor_id = min(length - 1, floor_id)
+    layout = mappa.floor_lists[mappa_idx][start_offset + floor_id].layout
+    tileset_id = layout.tileset_id
+    if tileset_id > 169:
+        tileset_id = 0
+    dma: Dma = dungeon_bin.get(f'dungeon{tileset_id}.dma')
+    dpl: Dpl = dungeon_bin.get(f'dungeon{tileset_id}.dpl')
+    dpla: Dpla = dungeon_bin.get(f'dungeon{tileset_id}.dpla')
+    dpci: Dpci = dungeon_bin.get(f'dungeon{tileset_id}.dpci')
+    dpc: Dpc = dungeon_bin.get(f'dungeon{tileset_id}.dpc')
+
+    fixedf = None
+    if level.mapty_enum == Pmd2ScriptLevelMapType.FIXED_ROOM:
+        fixedf = fixed.fixed_floors[layout.fixed_floor_id]
+
+    return dma, dpc, dpci, dpl, dpla, fixedf

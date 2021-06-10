143dbea9708ea32b1b51b5a0648a75fcec8fe3d6
Parakoopa <parakoopa@live.de>
2021-06-10 21:53:02 +0200

    Hardcoded main menu music

---
+++ skytemple_files/hardcoded/dbg/main_menu_music_test.py
@@ -0,0 +1,41 @@
+#  Copyright 2020-2021 Parakoopa and the SkyTemple Contributors
+# 
+#  This file is part of SkyTemple.
+# 
+#  SkyTemple is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  SkyTemple is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+import os
+
+from ndspy.rom import NintendoDSRom
+
+from skytemple_files.common.util import get_ppmdu_config_for_rom, get_binary_from_rom_ppmdu
+from skytemple_files.hardcoded.main_menu_music import MainMenuMusic
+
+base_dir = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..')
+rom_us = NintendoDSRom.fromFile(os.path.join(base_dir, 'skyworkcopy_us.nds'))
+rom_eu = NintendoDSRom.fromFile(os.path.join(base_dir, 'skyworkcopy.nds'))
+ppmdu_us = get_ppmdu_config_for_rom(rom_us)
+ppmdu_eu = get_ppmdu_config_for_rom(rom_eu)
+ov00_us = get_binary_from_rom_ppmdu(rom_us, ppmdu_us.binaries['overlay/overlay_0000.bin'])
+ov00_eu = get_binary_from_rom_ppmdu(rom_eu, ppmdu_us.binaries['overlay/overlay_0000.bin'])
+ov09_us = get_binary_from_rom_ppmdu(rom_us, ppmdu_us.binaries['overlay/overlay_0009.bin'])
+ov09_eu = get_binary_from_rom_ppmdu(rom_eu, ppmdu_us.binaries['overlay/overlay_0009.bin'])
+
+print(MainMenuMusic.get_main_menu_music(ov00_us, ppmdu_us, ov09_us))
+print(MainMenuMusic.get_main_menu_music(ov00_eu, ppmdu_eu, ov09_eu))
+
+MainMenuMusic.set_main_menu_music(123, ov00_us, ppmdu_us, ov09_us)
+MainMenuMusic.set_main_menu_music(4, ov00_eu, ppmdu_eu, ov09_eu)
+
+print(MainMenuMusic.get_main_menu_music(ov00_us, ppmdu_us, ov09_us))
+print(MainMenuMusic.get_main_menu_music(ov00_eu, ppmdu_eu, ov09_eu))
---
+++ skytemple_files/hardcoded/main_menu_music.py
@@ -0,0 +1,44 @@
+#  Copyright 2020-2021 Parakoopa and the SkyTemple Contributors
+#
+#  This file is part of SkyTemple.
+#
+#  SkyTemple is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SkyTemple is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+
+# Normal spawn delay (0x24): [EU]overlay_0010:0x7A74 / [US]overlay_0010:0x7A5C
+# Spawn delay after stealing from a shop (3): [EU]overlay_0010:0x7BD8 / [US]overlay_0010:0x7BC0
+from typing import Union, Tuple
+
+from skytemple_files.common.ppmdu_config.data import Pmd2Data
+from skytemple_files.common.util import read_uintle, write_uintle
+
+
+class MainMenuMusic:
+    @staticmethod
+    def get_main_menu_music(ov00: bytes, config: Pmd2Data, ov09: bytes = None) -> Union[int, Tuple[int, int]]:
+        """Set ov09 to also return the Sky Jukebox return music"""
+        main_block = config.binaries['overlay/overlay_0000.bin'].blocks['MainMenuSongId']
+        skyj_block = config.binaries['overlay/overlay_0009.bin'].blocks['MainMenuReturnSongId']
+        ov00_value = read_uintle(ov00, main_block.begin, 1)
+        if ov09:
+            return ov00_value, read_uintle(ov09, skyj_block.begin, 1)
+        return ov00_value
+
+    @staticmethod
+    def set_main_menu_music(value: int, ov00: bytearray, config: Pmd2Data, ov09: bytearray = None):
+        """Set ov09 to also update the Sky Jukebox return music"""
+        main_block = config.binaries['overlay/overlay_0000.bin'].blocks['MainMenuSongId']
+        skyj_block = config.binaries['overlay/overlay_0009.bin'].blocks['MainMenuReturnSongId']
+        write_uintle(ov00, value, main_block.begin, 1)
+        if ov09:
+            write_uintle(ov09, value, skyj_block.begin, 1)

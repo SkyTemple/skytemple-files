0bc1c4f13d79342e1879449149c4fc6118ba2322
irdkwia <irdkwia2000@outlook.com>
2021-10-03 00:50:18 +0200

    Anim file first version

---
+++ skytemple_files/data/anim/__init__.py
@@ -0,0 +1,23 @@
+#  Copyright 2020-2021 Capypara and the SkyTemple Contributors
+#
+#  This file is part of SkyTemple.
+#
+#  SkyTemple is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SkyTemple is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+
+HEADER_SIZE = 0x14
+TRAP_DATA_SIZE = 0x2
+ITEM_DATA_SIZE = 0x4
+MOVE_DATA_SIZE = 0x18
+GENERAL_DATA_SIZE = 0x1C
+SPECIAL_MOVE_DATA_SIZE = 0x6

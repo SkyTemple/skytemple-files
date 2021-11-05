409cd02c3d7cda23f5706399238c4c366b5cf06f
Parakoopa <parakoopa@live.de>
2021-11-05 19:51:52 +0100

    Version 1.3.3

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '1.3.2'
+__version__ = '1.3.3'
 import os
 
 from setuptools import setup, find_packages
@@ -54,6 +54,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )

f01d23cb6943acb1391d8820638095bedc7e2431
Parakoopa <parakoopa@live.de>
2021-11-30 23:07:10 +0100

    Correctly add py.typed to package data

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     version=__version__,
     packages=find_packages(),
     package_data={'skytemple_files':
-                  get_resources(['.xml', '.asm', '.rst', 'LICENSE', '.txt', 'md', '.bin', '.png', '.typed']) +
+                  get_resources(['.xml', '.asm', '.rst', 'LICENSE', '.txt', 'md', '.bin', '.png']) +
                   ['graphics/chara_wan/Shadow.png', 'py.typed']},
     description='Python library to edit the ROM of Pokémon Mystery Dungeon Explorers of Sky (EU/US)',
     long_description=long_description,

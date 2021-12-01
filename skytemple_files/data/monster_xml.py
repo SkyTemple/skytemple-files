4add0b212cee88dc0b99dcff56e78888b55694e1
irdkwia <43478422+irdkwia@users.noreply.github.com>
2021-12-01 18:04:28 +0100

    Edit Pokémon Idle Animation Type (#178)
    * Edit Pokémon Idle Animation Type
    
    * Replace value by enum
    
    * Fixed typing issues
    
    Co-authored-by: Parakoopa <parakoopa@live.de>

--- skytemple_files/data/monster_xml.py
+++ skytemple_files/data/monster_xml.py
@@ -18,12 +18,14 @@ Theoretically compatible with ppmdu, but contains some new field names for some 
 #
 #  You should have received a copy of the GNU General Public License
 #  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+# mypy: ignore-errors
 from abc import ABC, abstractmethod
 from base64 import b64encode, b64decode
 from typing import Optional, List, Tuple, Dict, Generic, TypeVar
 from xml.etree import ElementTree
 from xml.etree.ElementTree import Element
 
+from skytemple_files.common.types.file_types import FileType
 from skytemple_files.common.xml_util import validate_xml_tag, XmlValidateError
 from skytemple_files.data.level_bin_entry.model import LevelBinEntry, LevelEntry
 from skytemple_files.data.md.model import Md, MdEntry, EvolutionMethod, Gender, Ability, ShadowSize, PokeType, \
@@ -32,7 +34,6 @@ from skytemple_files.data.waza_p.model import MoveLearnset, LevelUpMove
 from skytemple_files.graphics.kao.model import KaoImage
 from skytemple_files.common.i18n_util import f, _
 
-
 XML_MONSTER = "Pokemon"
 XML_MONSTER__GAME_VERSION = "gameVersion"
 XML_STRINGS = "Strings"
@@ -40,6 +41,7 @@ XML_STRINGS__NAME = "Name"
 XML_STRINGS__CATEGORY = "Category"
 XML_GENENT = "GenderedEntity"
 XML_GENENT_ENTID = "PokeID"
+XML_GENENT_IDLE_ANIM = "IdleAnim"
 XML_GENENT_PERSONALITY = "Personality"
 XML_GENENT_UNK31 = "Unk31"
 XML_GENENT_NATIONAL_POKEDEX_NUMBER = "PokedexNumber"
@@ -210,9 +212,10 @@ class StringsXml(XmlConverter[Dict[str, Tuple[str, str]]]):
 
 
 class GenderedConvertEntry:
-    def __init__(self, md_entry: MdEntry, personality: Optional[int]):
+    def __init__(self, md_entry: MdEntry, personality: Optional[int], idle_anim: Optional[int]):
         self.md_entry = md_entry
         self.personality = personality
+        self.idle_anim = idle_anim
 
 
 class GenderedEntityXml(XmlConverter[GenderedConvertEntry]):
@@ -228,6 +231,9 @@ class GenderedEntityXml(XmlConverter[GenderedConvertEntry]):
         # Personality
         if base_value.personality is not None:
             xml.append(create_elem_w_text(XML_GENENT_PERSONALITY, base_value.personality))
+        # Idle Animation
+        if base_value.idle_anim is not None:
+            xml.append(create_elem_w_text(XML_GENENT_IDLE_ANIM, base_value.idle_anim))
         # Evolution requirements
         evo = Element(XML_GENENT_EVOLUTION_REQ)
         evo.append(create_elem_w_text(XML_GENENT_EVOLUTION_REQ__PRE_EVO_INDEX, value.pre_evo_index))
@@ -277,6 +283,8 @@ class GenderedEntityXml(XmlConverter[GenderedConvertEntry]):
                     setattr(value_to_update, attr_name, int(sub_xml.text))
             if sub_xml.tag == XML_GENENT_PERSONALITY:
                 base_value_to_update.personality = int(sub_xml.text)
+            if sub_xml.tag == XML_GENENT_IDLE_ANIM:
+                base_value_to_update.idle_anim = int(sub_xml.text)
             if sub_xml.tag == XML_GENENT_EVOLUTION_REQ:
                 pre_evo_index = None
                 method = None
@@ -578,8 +586,9 @@ class PortraitsXml(XmlConverter[List[Optional[KaoImage]]]):
         for kao in values:
             kao_xml = Element(XML_PORTRAITS_PORTRAIT)
             if kao is not None:
-                image = str(b64encode(kao.compressed_img_data), 'ascii')
-                pal = str(b64encode(kao.pal_data), 'ascii')
+                data = kao.raw()
+                image = str(b64encode(data[0]), 'ascii')
+                pal = str(b64encode(data[1]), 'ascii')
                 kao_xml.append(create_elem_w_text(XML_PORTRAITS_PORTRAIT__IMAGE, image))
                 kao_xml.append(create_elem_w_text(XML_PORTRAITS_PORTRAIT__PALETTE, pal))
             xml.append(kao_xml)
@@ -610,8 +619,8 @@ class PortraitsXml(XmlConverter[List[Optional[KaoImage]]]):
                         f(_("Invalid XML. '{XML_PORTRAITS_PORTRAIT__PALETTE}' missing for a portrait."))
                     )
                 try:
-                    value_to_update[i] = KaoImage(
-                        b64decode(palette.encode('ascii')) + b64decode(image.encode('ascii')), 0
+                    value_to_update[i] = FileType.KAO.get_image_model_cls().create_from_raw(
+                        b64decode(image.encode('ascii')), b64decode(palette.encode('ascii'))
                     )
                 except Exception as err:
                     raise XmlValidateError(
@@ -625,8 +634,9 @@ def monster_xml_export(game_version: str, md_gender1: Optional[MdEntry], md_gend
                        names: Optional[Dict[str, Tuple[str, str]]],
                        moveset: Optional[MoveLearnset], moveset2: Optional[MoveLearnset],
                        stats: Optional[LevelBinEntry],
-                       portraits: Optional[List[KaoImage]], portraits2: Optional[List[KaoImage]],
-                       personality1: Optional[int] = None, personality2: Optional[int] = None
+                       portraits: Optional[List[KaoImage]], portraits2: Optional[List[KaoImageProtocol]],
+                       personality1: Optional[int] = None, personality2: Optional[int] = None,
+                       idle_anim1: Optional[int] = None, idle_anim2: Optional[int] = None
                        ) -> ElementTree:
     """
     Exports properties of all given things as an XML file. If a second Md entry is given,
@@ -637,9 +647,9 @@ def monster_xml_export(game_version: str, md_gender1: Optional[MdEntry], md_gend
     if names:
         xml.append(StringsXml.to_xml(names))
     if md_gender1:
-        xml.append(GenderedEntityXml.to_xml(GenderedConvertEntry(md_gender1, personality1)))
+        xml.append(GenderedEntityXml.to_xml(GenderedConvertEntry(md_gender1, personality1, idle_anim1)))
     if md_gender2:
-        xml.append(GenderedEntityXml.to_xml(GenderedConvertEntry(md_gender2, personality2)))
+        xml.append(GenderedEntityXml.to_xml(GenderedConvertEntry(md_gender2, personality2, idle_anim2)))
     if moveset:
         xml.append(MovesetXml.to_xml(moveset))
     if moveset2:
@@ -658,7 +668,7 @@ def monster_xml_import(xml: ElementTree,
                        names: Optional[Dict[str, Tuple[str, str]]],
                        moveset: Optional[MoveLearnset], moveset2: Optional[MoveLearnset],
                        stats: Optional[LevelBinEntry],
-                       portraits: Optional[List[KaoImage]], portraits2: Optional[List[KaoImage]]) -> str:
+                       portraits: Optional[List[KaoImage]], portraits2: Optional[List[KaoImageProtocol]]) -> str:
     """
     Imports the available data from the XML into the models and lists given.
     The lists can already be filled, they will be cleared and re-filled when data is avaiable.
---
+++ skytemple_files/patch/handler/pkmn_ground_anim.py
@@ -0,0 +1,124 @@
+#  Copyright 2020-2021 Capypara and the SkyTemple Contributors
+#
+#  This file is part of SkyTemple.
+#
+#  SkyTemple is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SkyTemple is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+from typing import Callable, Dict, List, Set
+
+from ndspy.code import loadOverlayTable
+from ndspy.rom import NintendoDSRom
+
+from skytemple_files.common.util import *
+from skytemple_files.common.ppmdu_config.data import Pmd2Data, GAME_VERSION_EOS, GAME_REGION_US, GAME_REGION_EU, GAME_REGION_JP
+from skytemple_files.patch.category import PatchCategory
+from skytemple_files.patch.handler.abstract import AbstractPatchHandler
+from skytemple_files.patch.asm_tools import AsmFunction
+from skytemple_files.common.i18n_util import _
+
+START_OV11_US = 0x022DC240
+START_TABLE_US = 0x022F5D50
+LST_FUNC_US = [0x022F6E18, 0x022F6E20, 0x022F6E28, 0x022F6E44]
+
+START_OV11_EU = 0x022DCB80
+START_TABLE_EU = 0x022F66F0
+LST_FUNC_EU = [0x022F77B8, 0x022F77C0, 0x022F77C8, 0x022F77E4]
+
+START_OV11_JP = 0x022DD8E0
+START_TABLE_JP = 0x022F73D4
+LST_FUNC_JP = [0x022F849C, 0x022F84A4, 0x022F84AC, 0x022F84C8]
+
+PATCH_CHECK_ADDR_APPLIED_US = 0x19B10
+PATCH_CHECK_ADDR_APPLIED_EU = 0x19B70
+PATCH_CHECK_ADDR_APPLIED_JP = 0x19AF4
+PATCH_CHECK_INSTR_APPLIED = 0xE59F2F9C
+
+
+class PkmnGroundAnimPatchHandler(AbstractPatchHandler):
+
+    @property
+    def name(self) -> str:
+        return 'ChangePokemonGroundAnim'
+
+    @property
+    def description(self) -> str:
+        return _('Changes implementation of idle animation to an editable table.')
+
+    @property
+    def author(self) -> str:
+        return 'irdkwia'
+
+    @property
+    def version(self) -> str:
+        return '0.0.1'
+
+    @property
+    def category(self) -> PatchCategory:
+        return PatchCategory.UTILITY
+
+    def is_applied(self, rom: NintendoDSRom, config: Pmd2Data) -> bool:
+        if config.game_version == GAME_VERSION_EOS:
+            if config.game_region == GAME_REGION_US:
+                return read_uintle(rom.loadArm9Overlays([11])[11].data, PATCH_CHECK_ADDR_APPLIED_US, 4)!=PATCH_CHECK_INSTR_APPLIED
+            if config.game_region == GAME_REGION_EU:
+                return read_uintle(rom.loadArm9Overlays([11])[11].data, PATCH_CHECK_ADDR_APPLIED_EU, 4)!=PATCH_CHECK_INSTR_APPLIED
+            if config.game_region == GAME_REGION_JP:
+                return read_uintle(rom.loadArm9Overlays([11])[11].data, PATCH_CHECK_ADDR_APPLIED_JP, 4)!=PATCH_CHECK_INSTR_APPLIED
+        raise NotImplementedError()
+
+    def apply(self, apply: Callable[[], None], rom: NintendoDSRom, config: Pmd2Data):
+        if not self.is_applied(rom, config):
+            if config.game_version == GAME_VERSION_EOS:
+                if config.game_region == GAME_REGION_US:
+                    start_ov11 = START_OV11_US
+                    start_table = START_TABLE_US
+                    lst_func = LST_FUNC_US
+                if config.game_region == GAME_REGION_EU:
+                    start_ov11 = START_OV11_EU
+                    start_table = START_TABLE_EU
+                    lst_func = LST_FUNC_EU
+                if config.game_region == GAME_REGION_JP:
+                    start_ov11 = START_OV11_JP
+                    start_table = START_TABLE_JP
+                    lst_func = LST_FUNC_JP
+
+            table = loadOverlayTable(rom.arm9OverlayTable, lambda x,y:bytes())
+            ov = table[11]
+            ov11 = bytearray(rom.files[ov.fileID])
+            
+            switch = AsmFunction(ov11[start_table-start_ov11:lst_func[0]-start_ov11], start_table)
+            ext_data = switch.process()[1]
+            lst_data = {}
+            data_processed = set()
+            for offset in ext_data:
+                code = 0
+                for x in range(4):
+                    code += ov11[offset-start_ov11+x]*(256**x)
+                lst_data[offset] = code
+                data_processed.add(offset)
+            switch.provide_data(lst_data)
+            main_calls = switch.process_switch(0, (0,2048), {})
+            lst_calls = []
+            for x in main_calls:
+                lst_calls.append(lst_func.index(x))
+            ov11[start_table-start_ov11+4:start_table-start_ov11+2052] = bytes(lst_calls)
+            rom.files[ov.fileID] = bytes(ov11)
+            
+        try:
+            apply()
+        except RuntimeError as ex:
+            raise ex
+
+    
+    def unapply(self, unapply: Callable[[], None], rom: NintendoDSRom, config: Pmd2Data):
+        raise NotImplementedError()

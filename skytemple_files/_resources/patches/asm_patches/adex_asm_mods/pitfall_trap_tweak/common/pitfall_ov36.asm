9949b42e1395c590338c53c8f0dde2e8b13b9e39
Adex-8x <91418697+Adex-8x@users.noreply.github.com>
2022-05-29 21:52:49 -0400

    Add files via upload

---
+++ skytemple_files/_resources/patches/asm_patches/adex_asm_mods/pitfall_trap_tweak/common/pitfall_ov36.asm
@@ -0,0 +1,23 @@
+.org 0x023A7080+0x9F0 ; TODO: Ask End for a proper region
+.area 0xA20-0x9F0
+
+FallenAndCantGetUp:
+    
+	; r0 = Dereferenced DungeonBaseStructPtr
+	; r1 = 0x2
+	push r0-r12,r14
+	mov r4,r0
+	strb r1,[r4,6h] ; Advance to the next floor
+	ldrb r0,[r4,0x748] ; Dungeon ID
+	bl DungeonGoesUp
+	cmp r0,#0
+	beq fall_ret ; Dungeon is descending
+dungeon_is_ascending:
+	ldrb r0,[r4,0x749] ; Floor ID
+	subs r0,r0,#2
+	movmi r0,#0 ; Avoiding underflow
+	strb r0,[r4,0x749]
+fall_ret:
+	pop r0-r12,r15
+    .pool
+.endarea

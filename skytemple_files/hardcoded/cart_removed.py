672d19fa5f044c626b1bd5169b713e8413bd6a5b
Parakoopa <parakoopa@live.de>
2022-06-23 17:15:45 +0200

    Tests for compression performance with the cart_removed file

--- skytemple_files/hardcoded/cart_removed.py
+++ skytemple_files/hardcoded/cart_removed.py
@@ -27,6 +27,7 @@ from PIL import Image
 IMG_WIDTH = 256
 IMG_HEIGHT = 96
 
+
 class HardcodedCartRemoved:
     @staticmethod
     def get_cart_removed_data(arm9: bytes, config: Pmd2Data) -> Image.Image:
@@ -38,10 +39,10 @@ class HardcodedCartRemoved:
         img_data = CommonAtHandler.deserialize(data).decompress()
         raw_data = []
         for l, h in iter_bytes(img_data, 2):
-            v = l+h*256
-            raw_data.append((v%32)*8)
-            raw_data.append(((v>>5)%32)*8)
-            raw_data.append(((v>>10)%32)*8)
+            v = l + h * 256
+            raw_data.append((v % 32) * 8)
+            raw_data.append(((v >> 5) % 32) * 8)
+            raw_data.append(((v >> 10) % 32) * 8)
         return Image.frombytes(mode="RGB", size=(IMG_WIDTH, IMG_HEIGHT), data=bytes(raw_data))
 
     @staticmethod
@@ -49,17 +50,18 @@ class HardcodedCartRemoved:
         """
         Sets the cartridge removed data
         """
-        if img.width!=IMG_WIDTH and img.height!=IMG_HEIGHT:
+        if img.width != IMG_WIDTH and img.height != IMG_HEIGHT:
             raise AttributeError(f(_("The image must have dimensions {IMG_WIDTH}x{IMG_HEIGHT}.")))
         block = config.binaries['arm9.bin'].symbols['CartRemovedImgData']
         img = img.convert("RGB")
         raw_data = img.tobytes()
         img_data = []
         for r, g, b in iter_bytes(raw_data, 3):
-            v = (r//8) + ((g//8)<<5) + ((b//8)<<10)
-            img_data.append(v%256)
-            img_data.append(v//256)
+            v = (r // 8) + ((g // 8) << 5) + ((b // 8) << 10)
+            img_data.append(v % 256)
+            img_data.append(v // 256)
         data = CommonAtHandler.serialize(CommonAtHandler.compress(bytes(img_data), [CommonAtType.AT3PX]))
-        if len(data)>block.end-block.begin:
-            raise AttributeError(f(_("This image must be compressed better to fit in the arm9 ({len(data)} > {block.end-block.begin}).")))
-        arm9[block.begin:block.end] = data + bytes((block.end-block.begin)-len(data))
+        if len(data) > block.end - block.begin:
+            raise AttributeError(
+                f(_("This image must be compressed better to fit in the arm9 ({len(data)} > {block.end-block.begin}).")))
+        arm9[block.begin:block.end] = data + bytes((block.end - block.begin) - len(data))

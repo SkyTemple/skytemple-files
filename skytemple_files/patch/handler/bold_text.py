64f7af3c055434bd04d264973c2e88fe57dde471
Adex-8x <91418697+Adex-8x@users.noreply.github.com>
2022-08-12 20:28:23 -0400

    BoldText - I need to learn how to read

--- skytemple_files/patch/handler/bold_text.py
+++ skytemple_files/patch/handler/bold_text.py
@@ -63,9 +63,9 @@ class BoldTextPatchHandler(AbstractPatchHandler, DependantPatch):
     def is_applied(self, rom: NintendoDSRom, config: Pmd2Data) -> bool:
          if config.game_version == GAME_VERSION_EOS:
              if config.game_region == GAME_REGION_US:
-                 return read_u32(rom.arm9, OFFSET_US, 4) != ORIGINAL_INSTRUCTION
+                 return read_u32(rom.arm9, OFFSET_US) != ORIGINAL_INSTRUCTION
              if config.game_region == GAME_REGION_EU:
-                 return read_u32(rom.arm9, OFFSET_EU, 4) != ORIGINAL_INSTRUCTION
+                 return read_u32(rom.arm9, OFFSET_EU) != ORIGINAL_INSTRUCTION
          raise NotImplementedError()
 
     def apply(self, apply: Callable[[], None], rom: NintendoDSRom, config: Pmd2Data) -> None:

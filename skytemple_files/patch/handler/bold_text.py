da9baead98a69c73531b3698833e78b9263be3d1
Adex-8x <91418697+Adex-8x@users.noreply.github.com>
2022-08-12 20:09:56 -0400

    Updated to new format

--- skytemple_files/patch/handler/bold_text.py
+++ skytemple_files/patch/handler/bold_text.py
@@ -1,28 +1,34 @@
- #  Copyright 2020-2022 Capypara and the SkyTemple Contributors
- #
- #  This file is part of SkyTemple.
- #
- #  SkyTemple is free software: you can redistribute it and/or modify
- #  it under the terms of the GNU General Public License as published by
- #  the Free Software Foundation, either version 3 of the License, or
- #  (at your option) any later version.
- #
- #  SkyTemple is distributed in the hope that it will be useful,
- #  but WITHOUT ANY WARRANTY; without even the implied warranty of
- #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- #  GNU General Public License for more details.
- #
- #  You should have received a copy of the GNU General Public License
- #  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+# Copyright 2020-2022 Capypara and the SkyTemple Contributors
+#
+#  This file is part of SkyTemple.
+#
+#  SkyTemple is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SkyTemple is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+from __future__ import annotations
+
 from typing import Callable, List
 
 from ndspy.rom import NintendoDSRom
 
+from skytemple_files.common.ppmdu_config.data import (
+    GAME_REGION_EU,
+    GAME_REGION_US,
+    GAME_VERSION_EOS,
+    Pmd2Data,
+)
 from skytemple_files.common.util import *
-from skytemple_files.common.ppmdu_config.data import Pmd2Data, GAME_VERSION_EOS, GAME_REGION_US, GAME_REGION_EU
 from skytemple_files.patch.category import PatchCategory
 from skytemple_files.patch.handler.abstract import AbstractPatchHandler, DependantPatch
-from skytemple_files.common.i18n_util import f, _
 
 ORIGINAL_INSTRUCTION = 0xE12FFF37
 OFFSET_EU = 0x21D68
@@ -55,12 +61,11 @@ class BoldTextPatchHandler(AbstractPatchHandler, DependantPatch):
         return PatchCategory.NEW_MECHANIC
 
     def is_applied(self, rom: NintendoDSRom, config: Pmd2Data) -> bool:
-         arm9 = get_binary_from_rom_ppmdu(rom, config.binaries['arm9.bin'])
          if config.game_version == GAME_VERSION_EOS:
              if config.game_region == GAME_REGION_US:
-                 return read_uintle(arm9, OFFSET_US, 4) != ORIGINAL_INSTRUCTION
+                 return read_u32(rom.arm9, OFFSET_US, 4) != ORIGINAL_INSTRUCTION
              if config.game_region == GAME_REGION_EU:
-                 return read_uintle(arm9, OFFSET_EU, 4) != ORIGINAL_INSTRUCTION
+                 return read_u32(rom.arm9, OFFSET_EU, 4) != ORIGINAL_INSTRUCTION
          raise NotImplementedError()
 
     def apply(self, apply: Callable[[], None], rom: NintendoDSRom, config: Pmd2Data) -> None:

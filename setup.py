4b8aebb6f148d159cf8a7a0a568de61cda3040f3
Capypara <hello@capypara.de>
2023-08-24 20:25:59 +0200

    Limit skytemple-rust version to 1.5.x

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         'Pillow >= 6.1.0',
         'appdirs >= 1.4.0',
         'explorerscript >= 0.1.3',
-        'skytemple-rust >= 1.5.3',
+        'skytemple-rust >= 1.5.3, < 1.6.0',
         'pyobjc==9.1.1; sys_platform == "darwin"',
         'dungeon-eos==0.0.5',
         'typing_extensions >= 3.9; python_version < "3.9"'

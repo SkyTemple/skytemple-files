bf494af8ac5bd55fbc1b4c01362d197c8df07fdb
Adex-8x <totallynotadex@gmail.com>
2023-11-20 12:36:37 -0500

    Minor selector fix

--- skytemple_files/_resources/patches/asm_patches/adex_asm_mods/change_portrait/selector_overlay29.asm
+++ skytemple_files/_resources/patches/asm_patches/adex_asm_mods/change_portrait/selector_overlay29.asm
@@ -1,11 +1,11 @@
 .relativeinclude on
 
 .if PPMD_GameVer == GameVer_EoS_NA
-	.include "common/portrait_ov36.asm"
+	.include "common/portrait_ov29.asm"
 .elseif PPMD_GameVer == GameVer_EoS_EU
-	.include "common/portrait_ov36.asm"
+	.include "common/portrait_ov29.asm"
 .elseif PPMD_GameVer == GameVer_EoS_JP
-	.include "common/portrait_ov36.asm"
+	.include "common/portrait_ov29.asm"
 .endif
 
 .relativeinclude off

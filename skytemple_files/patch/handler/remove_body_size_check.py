30ec78a45359fcf0aab284cf01d7c76747ba3055
End45 <71291438+End45@users.noreply.github.com>
2024-05-12 19:06:30 +0200

    Remove unused import

--- skytemple_files/patch/handler/remove_body_size_check.py
+++ skytemple_files/patch/handler/remove_body_size_check.py
@@ -28,7 +28,7 @@ from skytemple_files.common.ppmdu_config.data import (
     GAME_VERSION_EOS,
     Pmd2Data,
 )
-from skytemple_files.common.util import get_binary_from_rom, read_u32
+from skytemple_files.common.util import read_u32
 from skytemple_files.patch.category import PatchCategory
 from skytemple_files.patch.handler.abstract import AbstractPatchHandler
 

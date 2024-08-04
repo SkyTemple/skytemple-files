f340e151b42f573f7fd2a7e48349a9bca56ca4d7
Irdkwia <irdkwia2000@outlook.com>
2024-08-04 16:54:16 +0200

    Add FastChimecho patch

---
+++ skytemple_files/_resources/patches/asm_patches/anonymous_asm_mods/fast_chimecho/na/offsets.asm
@@ -0,0 +1,29 @@
+; For use with ARMIPS
+; 2024/08/04
+; For Explorers of Sky All Versions
+; ------------------------------------------------------------------------------
+; Makes Chimecho Assembly Menu Faster
+; ------------------------------------------------------------------------------
+
+
+.relativeinclude on
+.nds
+.arm
+
+.definelabel LD1, 0x0238B4DC
+.definelabel LD2, 0x0238B514
+.definelabel LD3, 0x0238B538
+.definelabel LD4, 0x0238B568
+
+.definelabel AP1, 0x0238B584
+.definelabel AP2, 0x0238B5E8
+
+.definelabel RP1, 0x0238B604
+.definelabel RP2, 0x0238B60C
+.definelabel RP3, 0x0238B628
+.definelabel RP4, 0x0238B68C
+.definelabel RP5, 0x0238B6B0
+.definelabel RP6, 0x0238B6C4
+
+.definelabel DL1, 0x0238B774
+.definelabel DL2, 0x0238B868

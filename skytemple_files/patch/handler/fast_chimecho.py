a3860f4c3538ed31529f5474dc64cb52204a8c3a
Capypara <hello@capypara.de>
2024-08-06 15:48:45 +0200

    formatting, localization

--- skytemple_files/patch/handler/fast_chimecho.py
+++ skytemple_files/patch/handler/fast_chimecho.py
@@ -1,44 +1,51 @@
- #  Copyright 2020-2024 Capypara and the SkyTemple Contributors
- #
- #  This file is part of SkyTemple.
- #
- #  SkyTemple is free software: you can redistribute it and/or modify
- #  it under the terms of the GNU General Public License as published by
- #  the Free Software Foundation, either version 3 of the License, or
- #  (at your option) any later version.
- #
- #  SkyTemple is distributed in the hope that it will be useful,
- #  but WITHOUT ANY WARRANTY; without even the implied warranty of
- #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- #  GNU General Public License for more details.
- #
- #  You should have received a copy of the GNU General Public License
- #  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
+#  Copyright 2020-2024 Capypara and the SkyTemple Contributors
+#
+#  This file is part of SkyTemple.
+#
+#  SkyTemple is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SkyTemple is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SkyTemple.  If not, see <https://www.gnu.org/licenses/>.
 from typing import Callable
 
 from ndspy.rom import NintendoDSRom
 
-from skytemple_files.common.util import read_u32, get_binary_from_rom
-from skytemple_files.common.ppmdu_config.data import Pmd2Data, GAME_VERSION_EOS, GAME_REGION_US, GAME_REGION_EU, GAME_REGION_JP
+from skytemple_files.common.util import read_u32
+from skytemple_files.common.ppmdu_config.data import (
+    Pmd2Data,
+    GAME_VERSION_EOS,
+    GAME_REGION_US,
+    GAME_REGION_EU,
+    GAME_REGION_JP,
+)
 from skytemple_files.patch.category import PatchCategory
 from skytemple_files.patch.handler.abstract import AbstractPatchHandler
-from skytemple_files.common.i18n_util import f, _
+from skytemple_files.common.i18n_util import _
 
 ORIGINAL_INSTRUCTION = 0xE3A03003
 OFFSET_US = 0x139C
 OFFSET_EU = 0x139C
 OFFSET_JP = 0x139C
 
-class FastChimechoPatchHandler(AbstractPatchHandler):
 
+class FastChimechoPatchHandler(AbstractPatchHandler):
     @property
     def name(self) -> str:
-        return 'FastChimecho'
+        return "FastChimecho"
 
     @property
     def description(self) -> str:
-        return """Shorten Chimecho Team Assembly menu interactions.
-Adding, removing and making leader are now fast interactions that immediately go back to the team member selection menu."""
+        return _(
+            "Shorten Chimecho Team Assembly menu interactions. Adding, removing and making leader are now fast interactions that immediately go back to the team member selection menu."
+        )
 
     @property
     def category(self) -> PatchCategory:
@@ -46,11 +53,11 @@ Adding, removing and making leader are now fast interactions that immediately go
 
     @property
     def author(self) -> str:
-        return ''
+        return ""
 
     @property
     def version(self) -> str:
-        return '0.8.6'
+        return "0.8.6"
 
     def is_applied(self, rom: NintendoDSRom, config: Pmd2Data) -> bool:
         if config.game_version == GAME_VERSION_EOS:
@@ -63,8 +70,8 @@ Adding, removing and making leader are now fast interactions that immediately go
         raise NotImplementedError()
 
     def apply(self, apply: Callable[[], None], rom: NintendoDSRom, config: Pmd2Data) -> None:
-         # Apply the patch
-         apply()
+        # Apply the patch
+        apply()
 
     def unapply(self, unapply: Callable[[], None], rom: NintendoDSRom, config: Pmd2Data):
         raise NotImplementedError()

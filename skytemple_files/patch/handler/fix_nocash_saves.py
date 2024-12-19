7b09bdce46a516e748032ecfa20e9ddd65fd2d29
Chesyon <55322011+Chesyon@users.noreply.github.com>
2024-12-19 14:52:32 -0500

    Added check for old version of ExtraSpace
    FixNo$GbaSaves 1.0.0 crashes the game on old versions of ExtraSpace, so we need to check for those old versions and prompt the user to update accordingly.

--- skytemple_files/patch/handler/fix_nocash_saves.py
+++ skytemple_files/patch/handler/fix_nocash_saves.py
@@ -37,6 +37,8 @@ OFFSET_EU = 0x4ADD0
 OFFSET_US = 0x4AA98
 OFFSET_JP = 0x4AE00
 
+EXTRA_SPACE_OFFSET = 0xC6C+0x34
+EXTRA_SPACE_ORIGINAL_INSTRUCTION = 0xE3A00000
 
 class FixNocashSavesPatchHandler(AbstractPatchHandler, DependantPatch):
     @property
@@ -73,8 +75,11 @@ class FixNocashSavesPatchHandler(AbstractPatchHandler, DependantPatch):
         raise NotImplementedError()
 
     def apply(self, apply: Callable[[], None], rom: NintendoDSRom, config: Pmd2Data) -> None:
-        # Apply the patch
-        apply()
+        if 36 in rom.loadArm9Overlays([36]) and read_u32(rom.arm9, EXTRA_SPACE_OFFSET) == EXTRA_SPACE_ORIGINAL_INSTRUCTION: # if overlay36 exists, but the NitroMain hook to load it does not, we're on the old version of ExtraSpace
+            # TODO: prompt user to update ExtraSpace; fail to apply patch
+        else:
+            # Apply the patch 
+            apply()
 
     def unapply(self, unapply: Callable[[], None], rom: NintendoDSRom, config: Pmd2Data) -> None:
         raise NotImplementedError()

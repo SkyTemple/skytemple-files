b2c2c9810b4b232d262fe8a0f533c5aa3b3a78b3
Chesyon <55322011+Chesyon@users.noreply.github.com>
2025-01-07 09:40:03 -0500

    Update patches.py

--- skytemple_files/patch/patches.py
+++ skytemple_files/patch/patches.py
@@ -131,7 +131,7 @@ from skytemple_files.patch.handler.obj_table import ExtractObjectTablePatchHandl
 from skytemple_files.patch.handler.partners_trigger_hidden_traps import (
     PartnersTriggerHiddenTraps,
 )
-from skytemple_files.patch.handler.party_updates_map import PartyUpdatesMap
+from skytemple_files.patch.handler.party_updates_map import PartyUpdatesMapPatchHandler
 from skytemple_files.patch.handler.pitfall_trap_tweak import (
     PitfallTrapTweakPatchHandler,
 )
